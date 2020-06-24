fe6d14a8dc592c74a5fb2aae708538b3f1c60bb2
wwjjff <2553614101@qq.com>
2020-06-24 16:37:17 +0800

    考试安排模块提交加路由完整版

--- springclouddemo/examprovider/src/main/java/provider/ProviderApplication1.java
+++ springclouddemo/examprovider/src/main/java/provider/ProviderApplication1.java
@@ -1,4 +1,15 @@
-package provider.entity;
+package provider;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@MapperScan("provider.mapper")
 public class ProviderApplication1 {
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication1.class,args);
+    }
 }
--- springclouddemo/examprovider/src/main/java/provider/entity/examinfo.java
+++ springclouddemo/examprovider/src/main/java/provider/entity/examinfo.java
@@ -1,4 +1,87 @@
 package provider.entity;
 
 public class examinfo {
+private int id;
+private  String classNo;
+private  String course;
+private  String startime;
+private  String endtime;
+private  String place;
+
+
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getClassNo() {
+        return classNo;
+    }
+
+    public void setClassNo(String classNo) {
+        this.classNo = classNo;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getStartime() {
+        return startime;
+    }
+
+    public void setStartime(String startime) {
+        this.startime = startime;
+    }
+
+    public String getEndtime() {
+        return endtime;
+    }
+
+    public void setEndtime(String endtime) {
+        this.endtime = endtime;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+
+    @Override
+    public String toString() {
+        return "examinfo{" +
+                "id=" + id +
+                ", classNo='" + classNo + '\'' +
+                ", course='" + course + '\'' +
+                ", startime='" + startime + '\'' +
+                ", endtime='" + endtime + '\'' +
+                ", place='" + place + '\'' +
+                '}';
+    }
+
+    public examinfo(int id, String classNo, String course, String startime, String endtime, String place) {
+        this.id = id;
+        this.classNo = classNo;
+        this.course = course;
+        this.startime = startime;
+        this.endtime = endtime;
+        this.place = place;
+    }
+
+
+
 }
